0354eb120b8509ba25ce45278c81ddc27c3597a3
Carlos Lazo <clazo@berkeley.edu>
2018-10-15 14:00:02 -0700

    updated fetch requests

--- src/components/cards.js
+++ src/components/cards.js
@@ -4,7 +4,6 @@ class Cards extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      dataFetched: false
     };
   }
 
@@ -13,55 +12,66 @@ class Cards extends Component {
       return (
         <div key={index} className="children">
           <div className="grid">
-          <div className="title"> {x.annotatable.link_title} </div>
-          <img className="picture" src={x.annotatable.image_url} width="92.18" height="87.45"/>
-          <div className="fragment"> {x.fragment} </div>
-          <div className="annotation">
-          {x.annotations[0].body.dom.children.map(x => {
-            if (x === "") {
-              return null;
-            }
-            if (typeof x === "object" && x.children !== undefined) {
-              const Tag = x.tag
-              return (<Tag>{x.children.map(x => {
-                if (typeof x === "string") {
-                  return x;
-                } else if (typeof x === "object" && x.children !== undefined) {
-                  const ChildrenTag = x.tag;
-                  let attribute =
-                    x.attributes !== undefined
-                      ? x.attributes.href !== undefined
-                        ? x.attributes.href
-                        : null
-                      : null;
+            <div className="title"> {x.annotatable.link_title} </div>
+            <img
+              className="picture"
+              src={x.annotatable.image_url}
+              width="92.18"
+              height="87.45"
+            />
+            <div className="fragment"> {x.fragment} </div>
+            <div className="annotation">
+              {x.annotations[0].body.dom.children.map(x => {
+                if (x === "") {
+                  return null;
+                }
+                if (typeof x === "object" && x.children !== undefined) {
+                  const Tag = x.tag;
                   return (
-                    <ChildrenTag href={attribute}>
+                    <Tag>
                       {x.children.map(x => {
                         if (typeof x === "string") {
                           return x;
-                        } else if (x.tag === "br") {
-                          return <br/>
                         } else if (
                           typeof x === "object" &&
                           x.children !== undefined
                         ) {
-                          return x.children.map(x => {
-                            if (typeof x === "string") {
-                              return x;
-                            } else {
-                              return <br/>;
-                            }
-                          });
+                          const ChildrenTag = x.tag;
+                          let attribute =
+                            x.attributes !== undefined
+                              ? x.attributes.href !== undefined
+                                ? x.attributes.href
+                                : null
+                              : null;
+                          return (
+                            <ChildrenTag href={attribute}>
+                              {x.children.map(x => {
+                                if (typeof x === "string") {
+                                  return x;
+                                } else if (x.tag === "br") {
+                                  return <br />;
+                                } else if (
+                                  typeof x === "object" &&
+                                  x.children !== undefined
+                                ) {
+                                  return x.children.map(x => {
+                                    if (typeof x === "string") {
+                                      return x;
+                                    } else {
+                                      return <br />;
+                                    }
+                                  });
+                                }
+                              })}
+                            </ChildrenTag>
+                          );
                         }
                       })}
-                    </ChildrenTag>
+                    </Tag>
                   );
                 }
-              })
-
-            }</Tag>)}
-          })}
-          </div>
+              })}
+            </div>
           </div>
         </div>
       );
@@ -69,25 +79,10 @@ class Cards extends Component {
   }
 
 
-  componentDidUpdate(prevProps){
-    if (this.props.parsedDataObject !== prevProps.parsedDataObject){
-      if (this.props.parsedDataObject !== undefined){
-        if (this.props.parsedDataObject.length > 0 ){
-          if (this.state.dataFetched === false){
-            this.setState({dataFetched: true})
-          }
-        }
-      }
-    }
-
-  }
-
   render() {
     return (
       <div className="parent">
-        {this.props.parsedDataObject
-          ? this.renderDataObject()
-          : null}
+        {this.props.parsedDataObject ? this.renderDataObject() : null}
       </div>
     );
   }
